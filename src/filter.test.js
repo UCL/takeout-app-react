bbf8482f02d33431c8a4597e661a33b7655dcb35
David Guzman <d.guzman@ucl.ac.uk>
2022-04-26 15:11:30 +0100

    Fix bug affecting certain html files with parentNode.childNodes[3] undefined

---
+++ src/filter.test.js
@@ -0,0 +1,51 @@
+it('processes a valid html', () => {
+  const searches = [
+    {
+      parentNode: {
+        childNodes: [
+          {textContent: "bla"},
+          {textContent: "ble"},
+          {textContent: "bli"},
+          {textContent: "blo"}
+        ]
+      }
+    }
+  ]
+  const result = [...searches].filter((item) => {
+    const {childNodes} = item.parentNode;
+    return childNodes[0] !== undefined && childNodes[1] !== undefined && childNodes[3] !== undefined;
+  }).map((item) => {
+      return {
+        type: item.parentNode.childNodes[0].textContent,
+        query: item.parentNode.childNodes[1].textContent,
+        date: item.parentNode.childNodes[3].textContent
+      };
+    });
+  const expected = [ { type: 'bla', query: 'ble', date: 'blo' } ];
+  expect(result).toEqual(expected);
+});
+
+it('processes an invalid html', () => {
+  const searches = [
+    {
+      parentNode: {
+        childNodes: [
+          {textContent: "bla"},
+          {textContent: "ble"},
+          {textContent: "bli"},
+        ]
+      }
+    }
+  ]
+  const result = [...searches].filter((item) => {
+    const {childNodes} = item.parentNode;
+    return childNodes !== undefined && childNodes[0] !== undefined && childNodes[1] !== undefined && childNodes[3] !== undefined;
+  }).map((item) => {
+      return {
+        type: item.parentNode.childNodes[0].textContent,
+        query: item.parentNode.childNodes[1].textContent,
+        date: item.parentNode.childNodes[3].textContent
+      };
+    });
+  expect(result).toHaveLength(0);
+});
